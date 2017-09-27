c1bddd308b0d7a0cee0442ba65a7526e9135af7a
Jeremy Herring <j.herring.8989@gmail.com>
2017-09-27 16:22:43 -0500

    first working version

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react'
+
+
+class Header extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state ={
+			title:'The keywords are: ',
+			keywords:''
+		}
+
+	}
+
+	inputChange(event){
+		this.setState({keywords:event.target.value})
+		this.props.newsSearch(event.target.value)
+	}
+
+	render(){
+		return(
+			<header>
+				<div className="logo"
+				onClick={()=>console.log('click')}>
+				Logo</div>
+				<input onChange={this.inputChange.bind(this)}/>
+				<div>{this.state.title}</div>
+				<div>{this.state.keywords}</div>
+			</header>
+			)
+	}
+}
+
+export default Header;
